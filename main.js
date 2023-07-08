819cd94df72d9b4fe303066c632e7e85a0b9fce1
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-07-07 23:10:13 -0400

    added email and phone validation

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let inputs = document.querySelectorAll('input');
-console.log(inputs)
 
+// nly inputs that have been 'clicked' will show error messages
 inputs.forEach(input => input.addEventListener('focusout', e => {
     e.target.classList.add('clicked');
 }))
+
+let pass = document.querySelector('#pass');
+let passConfirm = document.querySelector('#pass-confirm');
