6375eef9f35519beff373cc8a9a185effacecfe3
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-07-08 22:58:04 -0400

    finished password validation

--- main.js
+++ main.js
@@ -7,4 +7,19 @@ inputs.forEach(input => input.addEventListener('focusout', e => {
 
 let pass = document.querySelector('#pass');
 let passConfirm = document.querySelector('#pass-confirm');
-passConfirm.addEventListener('keyUp', e => console.log(e.target.value));
+let passConfirmErr = document.querySelector('#pass-confirm + span');
+
+let passFields = [pass, passConfirm]
+passFields.forEach(field => field.addEventListener('keyup', () => {
+    const check = passwordsMatch();
+    if (!check){
+        passConfirmErr.textContent = '* Passwords do not match'
+    }
+    else{
+        passConfirmErr.textContent = '';
+    }
+}));
+
+function passwordsMatch(){
+    return (pass.value === passConfirm.value);
+}
